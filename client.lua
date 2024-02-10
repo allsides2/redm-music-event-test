65f3fa41f8a2c48669a987ba26cdc7409466feff
allsides <juniorespindola201@gmail.com>
2024-02-10 14:17:45 +0000

    count music

--- client.lua
+++ client.lua
@@ -1,6 +1,4 @@
-
 local music_events = {
-
     "SUS1_EXIT",
     "SUS1_FAIL",
     "WNT4_START",
@@ -10,17 +8,16 @@ local music_events = {
     -- ADD MORE https://github.com/femga/rdr3_discoveries/blob/master/audio%2Fmusic_events%2Fmusic_events.lua#L6-L14
 }
 
-
 local currentlyPlaying = false
 
 RegisterCommand("playmusic", function()
+    local count = 0
     if not currentlyPlaying then
         currentlyPlaying = true
         TriggerEvent("chatMessage", "SYSTEM", {255, 0, 0}, "Playing music events for 5 seconds each.")
-        
-
         for _, event in ipairs(music_events) do
-            TriggerEvent("chatMessage", "SYSTEM", {255, 0, 0}, "Music: "..event)
+            count = count + 1
+            TriggerEvent("chatMessage", "SYSTEM", {255, 0, 0}, "["..count.."] Music: "..event)
             Citizen.InvokeNative(0x1E5185B72EF5158A, event)  -- PREPARE_MUSIC_EVENT
             Citizen.InvokeNative(0x706D57B0F50DA710, event)  -- TRIGGER_MUSIC_EVENT
             Wait(15000)  -- Wait for 15 seconds
